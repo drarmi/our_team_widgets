377eb2a1d0033c48034c8d174da5752d6d992c8d
Дрогальчук Артур <ar.drogalchuk@gmail.com>
2024-01-28 15:01:20 +0200

    npm package added
    npm package added with grunt

---
+++ wp-content/plugins/our-team/our-team.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Plugin Name: Our Team
+ * Description: This plugin adds a custom "Our Team" widget to the Elementor website builder.
+ * Plugin URI: www.linkedin.com/in/artur-drohalchuk-4013b425a
+ * Author: Drohalchuk Artur
+ * Version: 1.0.0
+ * Author URI: www.linkedin.com/in/artur-drohalchuk-4013b425a
+ * Text Domain: our_team
+ * 
+ * 
+ * Elementor tested up to: 3.16.0
+ * Elementor Pro tested up to: 3.16.0
+ */
+
+if (!defined('ABSPATH')) {
+	exit; // Exit if accessed directly.
+}
